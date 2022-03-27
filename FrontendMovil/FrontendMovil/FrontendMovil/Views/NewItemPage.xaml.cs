41c904eab04c7faabfa0d4f715b4d5e64935045c
Karlinkd010 <karlinkd010@gmail.com>
2022-03-27 03:10:20 -0600

    InitialMovil

---
+++ FrontendMovil/FrontendMovil/FrontendMovil/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using FrontendMovil.Models;
+using FrontendMovil.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FrontendMovil.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
